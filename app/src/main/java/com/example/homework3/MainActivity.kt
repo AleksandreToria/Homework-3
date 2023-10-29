88cfafccc53a34f50a58159176e08965092ade31
Aleksandre Toria <aleksandretoria1@gmail.com>
2023-10-29 23:34:14 +0400

    1)Added a feature, so that the program still has all the fields filled when returning to MainActivity 2)Added comments for better understanding

--- app/src/main/java/com/example/homework3/MainActivity.kt
+++ app/src/main/java/com/example/homework3/MainActivity.kt
@@ -24,7 +24,11 @@ class MainActivity : AppCompatActivity() {
             binding.etUsername
         )
 
+//        Save button press logic
+
         binding.btnSave.setOnClickListener {
+
+//            Checking for Null or Blank
             if (areElementsNullOrBlank(elements)) {
                 Toast.makeText(
                     applicationContext,
@@ -34,6 +38,7 @@ class MainActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
+//            Checking for username length
             if (binding.etUsername.text!!.length < 10) {
                 Toast.makeText(
                     applicationContext,
@@ -43,18 +48,22 @@ class MainActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
+//            Checking for email validation
             if (!isEmailValid(binding.etEmail.text.toString())) {
                 Toast.makeText(applicationContext, "Please enter a valid email", Toast.LENGTH_LONG)
                     .show()
                 return@setOnClickListener
             }
 
+//            Checking for valid age
             if (!isAgeValid(binding.etAge.text.toString().toInt())) {
                 Toast.makeText(applicationContext, "Please enter valid age", Toast.LENGTH_LONG)
                     .show()
                 return@setOnClickListener
             }
 
+//            After every validation passing, program goes to second activity
+//            and sends the field data
             val intent = Intent(this, MainActivity2::class.java)
             for (element in elements) {
                 intent.putExtra(element.hint.toString(), element.text.toString())
@@ -62,33 +71,56 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+
+//        Reminder to long press
         binding.btnClear.setOnClickListener {
             Toast.makeText(applicationContext, "Please long press to clear", Toast.LENGTH_LONG)
                 .show()
         }
 
+//        Long click to empty the fields
         binding.btnClear.setOnLongClickListener {
             deleteFields(elements)
 
             true
         }
-
     }
 
 
+//    OnResume function, after which the fields are refilled with data
+    override fun onResume() {
+        super.onResume()
+
+        val email = intent.getStringExtra("Email")
+        val age = intent.getStringExtra("Age")
+        val userName = intent.getStringExtra("Username")
+        val firstName = intent.getStringExtra("First Name")
+        val lastName = intent.getStringExtra("Last Name")
+
+        binding.etAge.setText(age)
+        binding.etEmail.setText(email)
+        binding.etFirstName.setText(firstName)
+        binding.etLastName.setText(lastName)
+        binding.etUsername.setText(userName)
+    }
+
+//    Function for checking Null or Blank
     private fun areElementsNullOrBlank(inputs: Array<AppCompatEditText>): Boolean {
         return inputs.any { input -> input.text.isNullOrBlank() }
     }
 
+//    Email validation
     private fun isEmailValid(mail: String): Boolean {
         val trimmedMail = mail.trim()
         return trimmedMail.contains("@") && !trimmedMail.startsWith("@") && !trimmedMail.endsWith("@")
     }
 
+//    Age validation
     private fun isAgeValid(age: Int): Boolean {
         return age > 0
     }
 
+//    Field deletion
     private fun deleteFields(inputs: Array<AppCompatEditText>) {
         for (i in inputs) {
             i.setText("")
--- app/src/main/java/com/example/homework3/MainActivity2.kt
+++ app/src/main/java/com/example/homework3/MainActivity2.kt
@@ -8,25 +8,37 @@ import com.example.homework3.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
     private lateinit var binding: ActivityMain2Binding
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
+//        Getting data from MainActivity
         val email = intent.getStringExtra("Email")
         val age = intent.getStringExtra("Age")
         val userName = intent.getStringExtra("Username")
         val firstName = intent.getStringExtra("First Name")
         val lastName = intent.getStringExtra("Last Name")
 
-        binding.etAge.text = age
-        binding.etEmail.text = email
-        binding.etName.text = "$firstName $lastName"
-        binding.etUsername.text = userName
+//        Entering texts in the fields
+        binding.etAge.text = "Age: $age"
+        binding.etEmail.text = "Email: $email"
+        binding.etName.text = "Full name: $firstName $lastName"
+        binding.etUsername.text = "Username: $userName"
 
+//        After the click, returning to MainActivity and also sending back the data
+//        To refill the fields
         binding.btnAgain.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
+
+            intent.putExtra("Age", age)
+            intent.putExtra("Email", email)
+            intent.putExtra("Username", userName)
+            intent.putExtra("First Name", firstName)
+            intent.putExtra("Last Name", lastName)
+
             startActivity(intent)
         }
     }
